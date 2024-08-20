860be1ba6f0364863e7eda4ee09f6284e8d7168c
Adwaith M Shaju <adwaith467@gmail.com>
2024-08-20 17:33:44 +0530

    changes to /update-prompt

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,73 +1,12 @@
-'use client';
+import React, { Suspense } from 'react';
+import EditPrompt from '@components/EditPrompt';
 
-import {useEffect, useState} from 'react'
-import { useSession } from 'next-auth/react';
-import { useRouter,useSearchParams } from 'next/navigation';
-
-import { Form } from '@components/Form';
-
-const EditPrompt = () => {
-    const router = useRouter()
-    
-    const [submitting, setSubmitting] = useState(false)
-    const searchParams = useSearchParams()
-    const promptId = searchParams.get('id')
-
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: '',
-    })
-
-    useEffect(()=>{
-        const getPromptDetails = async ()=>{
-            const response = await fetch(`/api/prompt/${promptId}`)
-
-            const data = await response.json()
-            console.log(data)
-            setPost({
-                prompt : data.prompt,
-                tag: data.tag
-            })
-        }
-
-        promptId && getPromptDetails()
-    },[promptId])
-
-    const updatePrompt = async (e) =>{
-        e.preventDefault();
-        setSubmitting(true)
-        if(!promptId) return alert('Prompt ID not found')
-        try{
-            const response = await fetch(`/api/prompt/${promptId}`,{
-                method : 'PATCH',
-                body : JSON.stringify({
-                    prompt : post.prompt,
-                    tag : post.tag
-                })
-    
-            })
-
-            if(response.ok){
-                router.push('/')
-            }
-        }
-        catch(err){
-            console.log(err)
-            submitting(false)
-        }
-
-    }
-
+const EditPromptPage = () => {
   return (
-    <Form
-          type = 'Edit' 
-          post = {post}
-          setPost = {setPost}
-          submitting = {submitting}
-          handleSubmit = {updatePrompt}
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPrompt />
+    </Suspense>
+  );
+};
 
-    />
-  )
-}
-
-export default EditPrompt
+export default EditPromptPage;
